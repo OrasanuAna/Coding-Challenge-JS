9ae5f82f0804fb06d03c635d8282587336b31c17
OrasanuAna <orasanu.anamaria19@gmail.com>
2024-10-22 14:12:59 +0300

    Refactor code and add coding challenges 1, 2 and 3 - Refactor index.html to include a gradient background Closes #123

---
+++ js-part1/CodingChallenge1.js
@@ -0,0 +1,42 @@
+//JavaScript Fundamentals – Part 1
+
+//Coding Challenge #1
+
+/*Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+GOOD LUCK 😀
+
+*/
+
+// Store Mark's and John's mass and height in variables
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+// Calculate both BMIs using the formula
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+// Create a Boolean variable 'markHigherBMI'
+const markHigherBMI = BMIMark > BMIJohn ? true : false;
+
+// Log the results
+console.log("Mark's BMI:", BMIMark);
+console.log("John's BMI:", BMIJohn);
+console.log("Is Mark's BMI higher than John's?", markHigherBMI);
---
+++ js-part1/CodingChallenge3.js
@@ -0,0 +1,62 @@
+//Coding Challenge #3
+
+/*
+There are two gymnastics teams, Dolphins and Koalas. They compete against each
+other 3 times. The winner with the highest average score wins a trophy!
+Your tasks:
+
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition,
+and print it to the console. Don't forget that there can be a draw, so test for that
+as well (draw means they have the same average score)
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+team only wins if it has a higher score than the other team, and the same time a
+score of at least 100 points. Hint: Use a logical operator to test for minimum
+score, as well as multiple else-if blocks 😉
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+both teams have the same score and both have a score greater or equal 100
+points. Otherwise, no team wins the trophy
+Test data:
+
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+GOOD LUCK 😀
+
+*/
+
+//vectorul de elemente cu scorul
+const scoreDolphins = [96, 108, 89];
+const scoreKoalas = [88, 91, 110];
+
+//facem suma elementelor din vector
+const sumScoreDolphins = scoreDolphins.reduce(
+  (scoreDolphins, i) => scoreDolphins + i,
+  0
+);
+const sumScoreKoalas = scoreKoalas.reduce(
+  (scoreKoalas, i) => scoreKoalas + i,
+  0
+);
+
+//1
+//average score
+const avarageScoreDolphins = sumScoreDolphins / 3;
+const avarageScoreKoalas = sumScoreKoalas / 3;
+
+//2 + 3
+if (avarageScoreDolphins > avarageScoreKoalas && avarageScoreDolphins >= 100) {
+  console.log("Dolphins câștigă cu un scor de: " + avarageScoreDolphins);
+} else if (
+  avarageScoreDolphins < avarageScoreKoalas &&
+  avarageScoreKoalas >= 100
+) {
+  console.log("Koalas câștigă cu un scor de: " + avarageScoreKoalas);
+} else if (
+  avarageScoreDolphins === avarageScoreKoalas &&
+  avarageScoreDolphins >= 100
+) {
+  console.log("Egalitate, ambele echipe au scorul minim de 100.");
+} else {
+  console.log("Nicio echipă nu câștigă trofeul.");
+}
