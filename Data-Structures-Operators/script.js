11146e331081c9be3d15cb36542d805912d64df9
OrasanuAna <orasanu.anamaria19@gmail.com>
2024-11-03 09:50:39 +0200

    Add orderPizza method and demonstrate destructuring and rest/spread operators in script.js

--- Data-Structures-Operators/script.js
+++ Data-Structures-Operators/script.js
@@ -42,7 +42,52 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
+
+// 1) DESTRUCTURING
+
+// SPREAD, beacuse on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+// REST, beacuse on LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+//console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+//console.log(pizza, risotto, otherFood);
+
+//Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+//console.log(weekdays);
+
+//2) FUNCTIONS
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+
+restaurant.orderPizza('mushrooms');
+
+/*
+////////////////////////////////////////////////////////////////
+// The Spread Operator (...)
 
 //Real-word example
 const ingredients = [
@@ -67,7 +112,6 @@ restaurantCopy.name = 'La Ana';
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
 
-/*
 ////////////////////////////////////////////////////////////////
 
 const arr = [7, 8, 9];
@@ -97,7 +141,7 @@ console.log(...str);
 console.log(``);
 
 //console.log(Object.values(restaurant));
-/*
+
 ////////////////////////////////////////////////////////////////
 //Destructuring Objects
 restaurant.orderDelivery({
