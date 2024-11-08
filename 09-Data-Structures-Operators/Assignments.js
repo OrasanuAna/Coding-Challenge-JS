e51251f013bdc2418075132cda60c37965df5813
OrasanuAna <orasanu.anamaria19@gmail.com>
2024-11-08 20:42:30 +0200

    Add examples of Maps in Assignments.js and enhance script.js with Map fundamentals

--- 09-Data-Structures-Operators/Assignments.js
+++ 09-Data-Structures-Operators/Assignments.js
@@ -502,3 +502,21 @@ const uniqueKeywordsArr = [...uniqueKeywords];
 uniqueKeywords.clear();
 
 // ---------- 13.Maps: Fundamentals ----------
+// 13.1
+// Create a new book, but this time, as a Map. Assign it to the bookMap variable. Use this array as initial data:
+const book = new Map([
+  ['title', 'Clean Code'],
+  ['author', 'Robert C. Martin'],
+]);
+
+// 13.2
+// Set a new key in bookMap called pages, and assign it with a number 464.
+
+// 13.3
+// Get the title and author values from bookMap, and log to the console a string formatted like that: "${title} by ${author}".
+
+// 13.4
+// Get the size of bookMap, and log it to the console.
+
+// 13.5
+// Check if bookMap has the author key. and if so, log "The author of the book is known" to the console.
--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -65,6 +65,42 @@ const ordersSet = new Set([
 
 /*
 ////////////////////////////////////////////////////////////////
+// Maps: Fundamentals
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories')); //true
+rest.delete(2);
+console.log(rest);
+console.log(rest.size); // 7
+// rest.clear();
+// console.log(rest); // 0
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+
+rest.set(document.querySelector('h1'), 'Heading');
+
+console.log(rest);
+
+console.log(rest.get(arr));
+
+////////////////////////////////////////////////////////////////
 // Sets
 console.log(ordersSet); // {'Pasta', 'Pizza', 'Risotto'}
 console.log(new Set('Ana')); // {'A', 'n', 'a'}
