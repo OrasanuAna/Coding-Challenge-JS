339be80b580d8b1727a6206094e5c835cb928b8f
OrasanuAna <orasanu.anamaria19@gmail.com>
2024-11-11 12:49:40 +0200

    Enhance script.js with string manipulation examples, email normalization, and baggage check functionality

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -55,6 +55,72 @@ const restaurant = {
 };
 
 const airline = 'TAP Air Portugal';
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// Fix capitalization in name
+const passenger = 'mArIa'; // Maria
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+
+console.log(passengerCorrect);
+
+// Comparing email
+
+const email = 'orasanu.anamaria19@gmail.com';
+const loginEmail = '  Orasanu.Anamaria19@Gmail.Com \n'; // Corectat "AnamMria19" în "Anamaria19"
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+// console.log(trimmedEmail);
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// replacing
+
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'All passengers come to boarding door 23. Boarding door 23!';
+console.log(announcement.replace('door', 'gate'));
+console.log(announcement.replaceAll('door', 'gate'));
+console.log(announcement.replace(/door/g, 'gate'));
+
+// Booleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320')); // true
+console.log(plane.includes('Boeing')); // false
+console.log(plane.startsWith('Air')); // true
+console.log(plane.startsWith('Aib')); // false
+console.log(plane.startsWith('Airb')); // true
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the NEW Airbus family');
+}
+
+// Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome aboard!');
+  }
+};
+
+checkBaggage('I have a laptop, some Food and a pocket Knife');
+checkBaggage('Sockes and camera');
+checkBaggage('Got some snackes and a gun for protection');
+
+/*
+////////////////////////////////////////////////////////////////
+// Working With Strings - Part 1
+const airline = 'TAP Air Portugal';
 const plane = 'A320';
 
 console.log(plane[0]); // A
@@ -95,7 +161,7 @@ console.log(typeof new String('Ana'));
 
 console.log(typeof new String('Ana').slice(1));
 
-/*
+
 ////////////////////////////////////////////////////////////////
 const question = new Map([
   ['question', 'What is the best programming language in the word?'],
