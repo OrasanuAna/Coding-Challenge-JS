b342a604ded606591195a71a270e9157af119200
OrasanuAna <orasanu.anamaria19@gmail.com>
2024-11-11 14:21:15 +0200

    Add string manipulation functions and examples in script.js

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -54,6 +54,57 @@ const restaurant = {
   },
 };
 
+// Split and join
+console.log('a+very+nice+string'.split('+'));
+console.log('Orasanu Ana Maria'.split(' '));
+
+const [firstName, lastName] = 'Ana Orasanu'.split(' ');
+
+const newName = ['Miss', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('ana maria orasanu');
+
+const message = 'Go to gate 23!';
+console.log(message.padStart(25, '+'));
+console.log('Ana'.padStart(25, '+'));
+console.log('Ana'.padStart(25, '+').padEnd(35, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(12345678));
+console.log(maskCreditCard('87654032'));
+
+// Repeat
+const message2 = 'Bad waether...All Departues Delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'✈️'.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(25);
+
+/*
+////////////////////////////////////////////////////////////////
+// Working With Strings - Part 2
 const airline = 'TAP Air Portugal';
 
 console.log(airline.toLowerCase());
@@ -117,7 +168,6 @@ checkBaggage('I have a laptop, some Food and a pocket Knife');
 checkBaggage('Sockes and camera');
 checkBaggage('Got some snackes and a gun for protection');
 
-/*
 ////////////////////////////////////////////////////////////////
 // Working With Strings - Part 1
 const airline = 'TAP Air Portugal';
