85cfa039b81024ce6cc9492b28e651f1990214ed
OrasanuAna <orasanu.anamaria19@gmail.com>
2024-11-12 11:48:33 +0200

    Implement check-in functionality and passport handling in script.js

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const bookings = [];
 
 const createBooking = function (
@@ -27,3 +28,36 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
+*/
+
+const flight = 'LH234';
+const ana = {
+  name: 'Ana Orasanu',
+  passport: 12345678910,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Miss. ' + passenger.name;
+
+  if (passenger.passport === 12345678910) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, ana);
+console.log(flight);
+console.log(ana);
+
+// Is the same as doing...
+const flightNum = flight;
+const passenger = ana;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(ana);
+checkIn(flight, ana);
