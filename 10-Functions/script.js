678eb4a4939a52b9f03cf9fb8534020796cb9ad1
OrasanuAna <orasanu.anamaria19@gmail.com>
2024-11-12 13:25:25 +0200

    Add string transformation functions and event handling in script.js

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -28,7 +28,7 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
-*/
+
 
 const flight = 'LH234';
 const ana = {
@@ -61,3 +61,33 @@ const newPassport = function (person) {
 
 newPassport(ana);
 checkIn(flight, ana);
+*/
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// High-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+};
+document.body.addEventListener('click', high5);
+
+['Ana', 'Maria', 'Cristina'].forEach(high5);
