9ea90a5dc9640857cda7dd5e02be48b61cbc42b2
OrasanuAna <orasanu.anamaria19@gmail.com>
2024-11-13 12:43:43 +0200

    Implement bind method examples and event listener for plane purchase in script.js

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -159,3 +159,42 @@ book.apply(swiss, flightData);
 console.log(swiss);
 
 book.call(swiss, ...flightData);
+
+// Bind method
+// book.call(eurowings, 23, 'Felix Sima');
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Felix Sima');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Ana Maria Orasanu');
+bookEW23('Martha Cooper');
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+
+// lufthansa.buyPlane();
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
